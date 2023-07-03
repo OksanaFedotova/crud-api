a55956cec4bbc9557a3438d916ee693af5f613a6
OksanaFedotova <oxana-fedotova@yandex.ru>
2023-07-03 23:02:51 +0300

    feat: add npm command

--- __tests__/test.ts
+++ __tests__/test.ts
@@ -1,15 +1,20 @@
 import getGetResponse from "../src/app/models/getGetResponse";
 import getPostResponse from "../src/app/models/getPostResponse";
 import IUser from "../src/app/types/IUser";
+import http from "http";
 
 const user: IUser = {
   username: "string",
   age: 4234,
   hobbies: [],
 };
-
+beforeAll(() => {
+    jest.spyOn(http, "createServer").mockImplementation(jest.fn());
+  });
 describe("", () => {
-  test("Get all records with a GET api/users request (an empty array is expected)", () => {
+  test("Get all records with a GET api/users request (an empty array is expected)", async () => {
+    http.createServer();
+    expect(http.createServer).toBeCalled();
     const [data, statusCode] = getGetResponse("/api/users");
     expect(JSON.parse(data)).toEqual([]);
     expect(statusCode).toBe(200);
@@ -22,7 +27,7 @@ describe("", () => {
   test("GET api/user/{userId} request => the created record by its id", () => {
     const [messagePost, statusCodePost] = getPostResponse("/api/users", user);
     const id = JSON.parse(messagePost).id;
-    const [message, statusCode] = getGetResponse(`/api/users/${id}`)
+    const [message, statusCode] = getGetResponse(`/api/users/${id}`);
     expect(JSON.parse(message).id).toEqual(id);
     expect(statusCode).toBe(200);
   });
